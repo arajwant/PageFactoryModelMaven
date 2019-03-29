d341b497da53f93b42b4eae2cb089704070475a7
RajMac <raj@RajsMacBook-Pro.local>
2019-03-28 19:41:12 -0700

    Page Factory Model with CI

---
+++ src/main/java/com/crm/qa/pages/SignUpPage.java
@@ -0,0 +1,13 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.crm.qa.base.TestBase;
+
+public class SignUpPage extends TestBase{
+
+	WebDriver driver;
+	
+	
+	
+}
